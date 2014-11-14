e210ecb0a195009e88cb6acd8213ef6d3a8f3d9e
nemelnikov <melnikov.n.e@gmail.com>
2014-11-14 17:08:40 +0400

    Finish sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  include SessionsHelper
+ $all_users=User.count
+
+ def count_all_user
+ 	User.count
+ end
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,11 +3,14 @@ class StaticPagesController < ApplicationController
   end
 
   def help
+  	 
   end
 
   def about
+  	#$all_users=User.count
   end
   
   def contact
   end 
+
 end
